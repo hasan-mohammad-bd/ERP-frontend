7b0b29c8156c2281d6a03e480e82ebb69c799be0
Hasan Mohammad <hasan.md.east@gmail.com>
2024-07-25 11:43:41 +0600

    income-statement initilized

---
+++ src/store/services/accounts/api/income-statement/index.ts
@@ -0,0 +1,19 @@
+import { IncomeStatementRow } from "@/lib/validators/accounts/income-statement";
+import { accountApi } from "../..";
+
+const incomeStatementApi = accountApi.injectEndpoints({
+	endpoints: (build) => ({
+		getIncomeStatement: build.query<
+			{ data: IncomeStatementRow  },
+			string
+		>({
+			query: (params) => `reports/income-statement?${params}`,
+			providesTags: ["trial-balance"],
+		}),
+	}),
+	overrideExisting: false,
+});
+
+export const {
+	useGetIncomeStatementQuery
+} = incomeStatementApi;
