b973442e0a763c578cfc3a39e1e876e4c3416205
Muhammad Touhiduzzaman <58657283+TouhidZaman@users.noreply.github.com>
2024-08-21 17:59:53 +0600

    Add entry update fixes (#141)
    * journal update issue fixed
    
    * production build updated

---
+++ dist/assets/index-ea506046.js
@@ -0,0 +1,1 @@
+import{aD as s}from"./index-7daa6dd8.js";const r=s.injectEndpoints({endpoints:e=>({getWorkplaces:e.query({query:()=>"workplaces",providesTags:["workplaces"]})}),overrideExisting:!1}),{useGetWorkplacesQuery:p}=r;export{p as u};
