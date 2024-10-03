facb1696f45f2395d500303f45bc9a998bed73a7
Hasan Mohammad <89174358+hasan-mohammad-bd@users.noreply.github.com>
2024-10-03 18:06:22 +0600

    Improve Hrm dashboard UI  (#244)
    * added the deshboard UI
    
    * adding the chart
    
    * adding the build file
    
    * added build file
    
    * adding build file

--- dist/assets/index-c26338b0.js
+++
@@ -1,1 +0,0 @@
-import{ar as r}from"./index-9b71b1d1.js";const s=r.injectEndpoints({endpoints:e=>({getWorkplaces:e.query({query:()=>"workplaces",providesTags:["workplaces"]})}),overrideExisting:!1}),{useGetWorkplacesQuery:p}=s;export{p as u};
