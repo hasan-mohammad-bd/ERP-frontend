94f506e4bb00d361ea9eddb5677bcd7679050668
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2024-10-14 17:02:07 +0600

    insecure req bug fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,17 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		host: true,
-		port: 3000,
-	},
-	
-	
-	resolve: {
-
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
-	
+  plugins: [react()],
+  server: {
+    host: true,
+    // host: "0.0.0.0", // Accepts connections from any IP
+    port: 3000,
+    strictPort: true, // Ensures the port is not changed
+    open: true, // Opens browser on start (optional)
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
