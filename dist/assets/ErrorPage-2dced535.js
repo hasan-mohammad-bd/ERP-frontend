8677e7b3e68030ce9dcea0eb641961a0cc8fb1c3
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2024-10-15 18:25:42 +0600

    production updated

---
+++ dist/assets/ErrorPage-2dced535.js
@@ -0,0 +1,1 @@
+import{a as r}from"./index-a206b070.js";function o(){return r.jsx("div",{children:"ErrorPage"})}export{o as default};
--- dist/assets/ErrorPage-f0d7692b.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./index-c37fb998.js";function o(){return r.jsx("div",{children:"ErrorPage"})}export{o as default};
