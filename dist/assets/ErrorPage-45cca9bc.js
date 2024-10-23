afd08260e791a2f9ab4e1e2368dad71a026f32b7
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2024-10-23 15:12:51 +0600

    env's fixed

---
+++ dist/assets/ErrorPage-45cca9bc.js
@@ -0,0 +1,1 @@
+import{a as r}from"./index-1c1a7b38.js";function o(){return r.jsx("div",{children:"ErrorPage"})}export{o as default};
--- dist/assets/ErrorPage-ab96bf38.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./index-736d57d7.js";function o(){return r.jsx("div",{children:"ErrorPage"})}export{o as default};
