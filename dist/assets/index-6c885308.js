19f3e728b4daa4cd88233b92d51d26f23a8fef57
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2024-10-28 15:18:22 +0600

    production build updated

--- dist/assets/index-6c885308.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:dist/assets/index-96d929cd.js
-import{a as e,L as s,B as t}from"./index-eb217bbf.js";const o=()=>e.jsxs("div",{className:"flex flex-col items-center justify-center pt-10",children:[e.jsx("img",{src:"/image/404page.png",className:"w-[700px]"}),e.jsx("h3",{className:"font-[500] mt-3 text-4xl",children:"Oops! The Page You're Looking For Doesn't Exist."}),e.jsx(s,{to:"/",className:"pt-10",children:e.jsx(t,{className:"px-7 py-6",children:"Back To Home"})})]});export{o as default};
-========
-import{a as e,L as s,B as t}from"./index-78a1e1b1.js";const o=()=>e.jsxs("div",{className:"flex flex-col items-center justify-center pt-10",children:[e.jsx("img",{src:"/image/404page.png",className:"w-[700px]"}),e.jsx("h3",{className:"font-[500] mt-3 text-4xl",children:"Oops! The Page You're Looking For Doesn't Exist."}),e.jsx(s,{to:"/",className:"pt-10",children:e.jsx(t,{className:"px-7 py-6",children:"Back To Home"})})]});export{o as default};
->>>>>>>> main:dist/assets/index-6c885308.js
