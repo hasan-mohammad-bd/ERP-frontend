3f32003f91675940c5528bccf86053e75dc0709e
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2024-11-05 15:24:12 +0600

    production build updated

--- dist/assets/ErrorPage-cd7f62af.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./index-8b8c1713.js";function o(){return r.jsx("div",{children:"ErrorPage"})}export{o as default};
---
+++ dist/assets/index-dfd797d7.js
@@ -0,0 +1,1 @@
+import{b9 as s}from"./index-96aa6580.js";const r=s.injectEndpoints({endpoints:e=>({getWorkplaces:e.query({query:()=>"workplaces",providesTags:["workplaces"]})}),overrideExisting:!1}),{useGetWorkplacesQuery:p}=r;export{p as u};
