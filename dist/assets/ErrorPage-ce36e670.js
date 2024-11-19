de1e54a35171d30641927772c53798c94cb53447
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2024-11-19 17:53:38 +0600

    production build updated

--- dist/assets/ErrorPage-ce36e670.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./index-48f4f9b1.js";function o(){return r.jsx("div",{children:"ErrorPage"})}export{o as default};
--- dist/assets/leave-summay-6a13e948.js
+++
@@ -1,1 +0,0 @@
-import{bi as r}from"./index-48f4f9b1.js";const s=r.injectEndpoints({endpoints:e=>({getLeaveSummary:e.query({query:a=>`leave-reports/summary?per_page=5&${a}`,providesTags:["leave-summary"]})}),overrideExisting:!1}),{useGetLeaveSummaryQuery:u,useLazyGetLeaveSummaryQuery:t}=s;export{u as a,t as u};
