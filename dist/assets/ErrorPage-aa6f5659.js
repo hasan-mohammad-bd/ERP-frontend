661d54a6a2e99e00f9cc1a8a0b015e79a17bf03c
Hasan Mohammad <hasan.md.east@gmail.com>
2025-01-01 17:11:00 +0600

    added build file

--- dist/assets/ErrorPage-aa6f5659.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./index-fe9eec72.js";function o(){return r.jsx("div",{children:"ErrorPage"})}export{o as default};
---
+++ dist/assets/ErrorPage-e72a87ce.js
@@ -0,0 +1,1 @@
+import{a as r}from"./index-7f58ec13.js";function o(){return r.jsx("div",{children:"ErrorPage"})}export{o as default};
