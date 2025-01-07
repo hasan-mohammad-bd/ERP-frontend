a62177a39788c92fbd58defc1f66c1ede984d19c
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2025-01-07 11:33:05 +0600

    build updated

--- dist/assets/ErrorPage-9b9da571.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./index-8da8ea63.js";function o(){return r.jsx("div",{children:"ErrorPage"})}export{o as default};
---
+++ dist/assets/index-3628fcd5.js
@@ -0,0 +1,1 @@
+import{bp as i}from"./index-c5db791d.js";const n=i.injectEndpoints({endpoints:o=>({getOrganizationForDropDown:o.query({query:()=>"self/organization",providesTags:["self-organization"]})}),overrideExisting:!1}),{useGetOrganizationForDropDownQuery:e}=n;export{e as u};
