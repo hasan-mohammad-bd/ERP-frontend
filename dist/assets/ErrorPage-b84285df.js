a45c9699cf9d343fe5fc0c1f4a3ff5f2de07b22d
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2025-01-30 18:03:45 +0600

    agrovet build added

---
+++ dist/assets/ErrorPage-b84285df.js
@@ -0,0 +1,1 @@
+import{a as r}from"./index-debe1a3c.js";function o(){return r.jsx("div",{children:"ErrorPage"})}export{o as default};
--- dist/assets/ErrorPage-d818a4a8.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./index-e9c6f511.js";function o(){return r.jsx("div",{children:"ErrorPage"})}export{o as default};
