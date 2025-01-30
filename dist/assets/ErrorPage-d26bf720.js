fcbe04f2eafe145d31088185c64e6f8214805ddc
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2025-01-30 21:25:37 +0600

    build updated

--- dist/assets/ErrorPage-d26bf720.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./index-224e0a71.js";function o(){return r.jsx("div",{children:"ErrorPage"})}export{o as default};
---
+++ dist/assets/index-5b957ea6.js
@@ -0,0 +1,1 @@
+import{bp as e}from"./index-929f8c19.js";const s=e.injectEndpoints({endpoints:t=>({getEmploymentStatuses:t.query({query:()=>"employment-status",providesTags:["employment-status"]})}),overrideExisting:!1}),{useGetEmploymentStatusesQuery:o,useLazyGetEmploymentStatusesQuery:n}=s;export{n as a,o as u};
