27fad9f7c53ae10487c5b136e3f961b90d319674
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2025-01-30 21:07:38 +0600

    build updated

---
+++ dist/assets/ErrorPage-d26bf720.js
@@ -0,0 +1,1 @@
+import{a as r}from"./index-224e0a71.js";function o(){return r.jsx("div",{children:"ErrorPage"})}export{o as default};
--- dist/assets/index-bdb0aa32.js
+++
@@ -1,1 +0,0 @@
-import{bp as s}from"./index-95d528fa.js";const r=s.injectEndpoints({endpoints:e=>({getGenders:e.query({query:()=>"genders",providesTags:["genders"]})}),overrideExisting:!1}),{useGetGendersQuery:t,useLazyGetGendersQuery:d}=r;export{d as a,t as u};
