bcd7be8cd9272f89ff9d8483b13e9f7b7f5ceb90
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2025-02-03 12:59:36 +0600

    agrovet build added

---
+++ dist/assets/index-c40a6e65.js
@@ -0,0 +1,1 @@
+import{bq as s}from"./index-68967a8a.js";const r=s.injectEndpoints({endpoints:e=>({getWorkplaces:e.query({query:()=>"workplaces",providesTags:["workplaces"]})}),overrideExisting:!1}),{useGetWorkplacesQuery:p}=r;export{p as u};
